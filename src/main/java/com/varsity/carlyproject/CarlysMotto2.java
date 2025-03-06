66259cd457ddb063507edf711295a53f1d4ddda7
St10467155-Neo-Mokoena <St10467155@vcconnect.edu.za>
2025-03-06 14:18:34 +0200

    Sec commit

---
+++ src/main/java/com/varsity/carlyproject/CarlysMotto2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.varsity.carlyproject;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class CarlysMotto2 {
+    
+    public static void main(String[]args){
+        System.out.println("****************************************************");
+        System.out.println("Even a mandela quote is fine think of something");
+        System.out.println("****************************************************");
+        
+        
+    }
+    
+}
